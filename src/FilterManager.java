ba58c50bef6d37207760be821622c61157b18218
Cláudio Margulhano <claudiomargulhano.verity@sulamerica.com.br>
2021-08-19 18:53:07 -0300

    first commit

---
+++ src/FilterManager.java
@@ -0,0 +1,16 @@
+public class FilterManager {
+    FilterChain chain;
+
+    public FilterManager(Target target) {
+        chain = new FilterChain();
+        chain.setTarget(target);
+    }
+
+    public void setFilter(Filter filter) {
+        chain.addFilter(filter);
+    }
+
+    public void filterRequest(String request) {
+        chain.execute(request);
+    }
+}
